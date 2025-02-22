7b0f5fda85249fa058baa59c3ad3067dde214763
Mn <maestro97041@gmail.com>
2025-02-22 14:26:47 +0300

    Добавляем файл для запуска программы

---
+++ main.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+from app import start_calculator  # Импортируем функцию для запуска интерфейса калькулятора
+
+if __name__ == "__main__":
+    # Запускаем калькулятор
+    start_calculator()
